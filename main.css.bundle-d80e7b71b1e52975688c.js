0a90029c42b83ce6fe259c98e68726a4b25f664e
Viatcheslav Zadorozhniy <viatcheslav.zadorozhniy@erbis.cloud>
2021-04-08 13:19:28 +0300

    Upload test website

---
+++ main.css.bundle-d80e7b71b1e52975688c.js
@@ -0,0 +1,6 @@
+webpackJsonp([3],{5:/*!**************************!*\
+  !*** ./styles/main.scss ***!
+  \**************************/
+/*! dynamic exports provided */
+/*! all exports used */
+function(n,c){}},[5]);
---
+++ svg-icons.css.bundle-d80e7b71b1e52975688c.js
@@ -0,0 +1,6 @@
+webpackJsonp([2],{6:/*!*******************************!*\
+  !*** ./styles/svg-icons.scss ***!
+  \*******************************/
+/*! dynamic exports provided */
+/*! all exports used */
+function(n,c){}},[6]);
